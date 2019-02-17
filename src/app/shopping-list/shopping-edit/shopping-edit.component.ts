5a04ae9cc5b688a2500de9c3fec953a96ce8b785
maharoz <mahruj66@gmail.com>
2019-02-17 21:01:02 +0600

    allowing user to clear or cancel the form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -47,6 +47,11 @@ export class ShoppingEditComponent implements OnInit,OnDestroy {
     form.reset();
   }
 
+  onClear(){
+    this.slForm.reset();
+    this.editMode = false;
+    
+  }
   ngOnDestroy(){
     this.subscription.unsubscribe();
   }
