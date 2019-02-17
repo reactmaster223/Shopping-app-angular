889486e78014ee5cef17de37855be93db6078487
maharoz <mahruj66@gmail.com>
2019-02-17 20:57:55 +0600

    form resetting done

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -34,7 +34,7 @@ export class ShoppingEditComponent implements OnInit,OnDestroy {
     });
   }
 
-  onAddItem(form : NgForm){
+  onSubmit(form : NgForm){
     const value= form.value;
     console.log(value);
     const newIngredient = new Ingredient(value.name,value.amount);
@@ -43,7 +43,8 @@ export class ShoppingEditComponent implements OnInit,OnDestroy {
     }else{
       this.slService.addIngredient(newIngredient);
     }
-    
+    this.editMode = false;
+    form.reset();
   }
 
   ngOnDestroy(){
