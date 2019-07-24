f21ca5731ee546078c3523184725b5b57a0d6f80
maharoz <mahruj66@gmail.com>
2019-07-24 23:58:23 +0600

    Adding navigation with ng if

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Shopping-App';
+ loadedFeature = 'recipe';
+  onNavigate(feature:string){
+    this.loadedFeature = feature;
+  }
 }
