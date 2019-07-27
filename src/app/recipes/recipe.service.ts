1fc34a025bfa0952480bb8019f812bb0822b8e43
maharoz <mahruj66@gmail.com>
2019-07-27 22:07:22 +0600

    added ingredients to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,28 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 
 export class RecipeService{
 
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('test recipe','simple test recipe','https://cdn.pixabay.com/photo/2017/06/21/22/42/recipe-2428926_960_720.jpg'),
-        new Recipe('test','simple test','https://cdn.pixabay.com/photo/2017/06/21/22/42/recipe-2428926_960_720.jpg')
+        new Recipe(
+            'test recipe',
+            'simple test recipe',
+            'https://cdn.pixabay.com/photo/2017/06/21/22/42/recipe-2428926_960_720.jpg',
+            [
+                new Ingredient('Meat',1),
+                new Ingredient('French Fry',20),
+            ]),
+        new Recipe(
+            'test',
+            'simple test',
+            'https://cdn.pixabay.com/photo/2017/06/21/22/42/recipe-2428926_960_720.jpg',
+            [
+                new Ingredient('Buns',2),
+                new Ingredient('Meat',1),
+            ])
      ]; 
 
      getRecipes(){
