f7fc83492bbcb788224ca73504648c70cbfcc7fc
maharoz <mahruj66@gmail.com>
2019-07-28 20:42:26 +0600

    router outlet added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/recipes',pathMatch:'full'},
+  {path : 'recipes', component:RecipesComponent},
+  {path : 'shopping-list', component:ShoppingListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
