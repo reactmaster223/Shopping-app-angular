c937e69894cc8bfa91ffe44d755ca6acde0e2c65
maharoz <mahruj66@gmail.com>
2019-08-10 16:29:19 +0600

    firebase add

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,21 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
     selector:'app-header',
     templateUrl:'./header.component.html'
 })
 export class HeaderComponent{
+
+  constructor(private dataStorageService:DataStorageService){
+
+  }
   //  @Output() featureSelected = new EventEmitter<string>();
 
     // onSelect(feature:string){
     //     this.featureSelected.emit(feature);
     // }
+    onSaveData(){
+  this.dataStorageService.storeRecipe();
+    }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({providedIn:'root'})
 export class DataStorageService{
-    constructor(private http:HttpClient){
-        
+    constructor(private http:HttpClient,
+        private recipeService :RecipeService){
+
     }
 
+
+    storeRecipe(){
+        const recipes = this.recipeService.getRecipes();
+        return this.http.put('https://ng-course-recipe-book-1f277.firebaseio.com/recipes.json',recipes).subscribe(
+            response =>{
+                console.log(response);
+            }
+        );
+    }
 }
