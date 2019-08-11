a0df1c4a2b8530c5d796b1fa367e57bd7cdaa653
maharoz <mahruj66@gmail.com>
2019-08-11 11:43:50 +0600

    adding error handler logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 import { RecipeService } from './recipes/recipe.service';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthComponent } from './auth/auth.component';
+import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { AuthComponent } from './auth/auth.component';
     DropdownDirective,
     RecipeStartComponent,
     RecipeEditComponent,
-    AuthComponent
+    AuthComponent,
+    LoadingSpinnerComponent
   ],
   imports: [
     BrowserModule,
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -8,6 +8,8 @@ import { AuthService } from './auth.service';
 })
 export class AuthComponent{
     isLoginMode = true;
+    isLoading = false;
+    error:string = null;
 constructor(private authService:AuthService){
 
 }
@@ -25,13 +27,17 @@ constructor(private authService:AuthService){
       const email = form.value.email;
       const password = form.value.password;
 
+      this.isLoading = true;
       if(this.isLoginMode){
 
       }else{
         this.authService.signup(email,password).subscribe(resData=>{
-            console.log(resData) 
+            console.log(resData);
+            this.isLoading = false;
           },error=>{
               console.log(error);
+              this.error = 'An error occured!';
+              this.isLoading =false;
           });
       }
       form.reset();
