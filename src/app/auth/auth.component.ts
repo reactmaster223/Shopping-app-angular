f5cd4db7e23061bf806cc6c73947e1c465bfdbe5
maharoz <mahruj66@gmail.com>
2019-08-11 10:48:37 +0600

    handling form input

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
    selector :'app-auth',
@@ -11,5 +12,10 @@ export class AuthComponent{
     onSwithMode(){
         this.isLoginMode =  !this.isLoginMode;
         //console.log(this.isLoginMode);
+    }
+
+    onSubmit(form:NgForm){
+      console.log(form.value);
+      form.reset();
     }
 }
