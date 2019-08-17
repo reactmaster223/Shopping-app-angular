2715a1ee46750b1ba3ed0641f5694aed248b61f2
maharoz <mahruj66@gmail.com>
2019-08-17 22:17:28 +0600

    Creating and storing user data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError, Subject } from 'rxjs';
+import { User } from './user.model';
 
 export interface AuthResponseData{
     kind : string;
@@ -15,6 +16,8 @@ export interface AuthResponseData{
 @Injectable({providedIn :'root'})
 export class AuthService{
 
+   user = new Subject<User>();
+   
     constructor(private http:HttpClient){
 
     }
@@ -24,7 +27,9 @@ export class AuthService{
        email: email,
        password:password,
        returnSecureToken:true
-    }).pipe(catchError(this.handleError));
+    }).pipe(catchError(this.handleError),tap(resData =>{
+     this.handleAuthentication(resData.email,resData.localId,resData.idToken,+resData.expiresIn)
+    }));
   }
 
 
@@ -34,9 +39,21 @@ export class AuthService{
       email: email,
       password:password,
       returnSecureToken:true
-   }).pipe(catchError(this.handleError));;
+   }).pipe(catchError(this.handleError),tap(resData =>{
+    this.handleAuthentication(resData.email,resData.localId,resData.idToken,+resData.expiresIn)
+   }));
   }
 
+
+  handleAuthentication(email:string,userId:string ,token:string,expiresIn:number){
+    const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
+    const user = new User(
+      email,
+      userId,
+      token,
+      expirationDate);
+      this.user.next(user);
+  }
 
   private handleError(errorRes : HttpErrorResponse){
     let errorMessage = 'An unknown errr occured';
