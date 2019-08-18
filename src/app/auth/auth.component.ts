3fed5aa8889ccf24a5b1d5c8cb502f5d21e3af89
maharoz <mahruj66@gmail.com>
2019-08-18 20:34:32 +0600

    Reflecting the ui change of authentication

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService ,AuthResponseData } from './auth.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
    selector :'app-auth',
@@ -11,7 +12,7 @@ export class AuthComponent{
     isLoginMode = true;
     isLoading = false;
     error:string = null;
-constructor(private authService:AuthService){
+constructor(private authService:AuthService,private router:Router){
 
 }
 
@@ -42,6 +43,7 @@ constructor(private authService:AuthService){
       authObs.subscribe(resData=>{
         console.log(resData);
         this.isLoading = false;
+        this.router.navigate(['/recipes']);
       },errorMessage=>{
           console.log(errorMessage);
         this.error = errorMessage;
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,16 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector:'app-header',
     templateUrl:'./header.component.html'
 })
-export class HeaderComponent{
-
-  constructor(private dataStorageService:DataStorageService){
+export class HeaderComponent implements OnInit,OnDestroy{
+  isAuthenticated = false;
+  private userSub : Subscription;
+  constructor(private dataStorageService:DataStorageService,private authService:AuthService){
 
   }
   //  @Output() featureSelected = new EventEmitter<string>();
@@ -15,11 +18,25 @@ export class HeaderComponent{
     // onSelect(feature:string){
     //     this.featureSelected.emit(feature);
     // }
+
+    ngOnInit(){
+     this.userSub = this.authService.user.subscribe(user =>{
+     this.isAuthenticated = !!user;
+     
+     });
+    }
+
+
+
     onSaveData(){
        this.dataStorageService.storeRecipe();
     }
 
     onfetchData(){
       this.dataStorageService.fetchRecipe().subscribe();
+    }
+
+    ngOnDestroy(){
+      this.userSub.unsubscribe();
     }
 }
