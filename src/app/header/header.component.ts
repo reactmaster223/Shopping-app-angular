cfa8b705c2dac115c4b98513493b1022cd1f5f1a
Maharoz <mahruj66@gmail.com>
2022-10-14 03:28:05 +0600

    lesson 304 completed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -25,6 +25,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this.dataStorageService.storeRecipes();
   }
 
+  onLogout() {
+    this.authService.logout();
+  }
+
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
   }
