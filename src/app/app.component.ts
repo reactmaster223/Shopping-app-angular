ae58f0a3ef18f6d9ca4b1511cc80db7da61e8bf1
Maharoz <mahruj66@gmail.com>
2022-10-14 10:25:28 +0600

    lesson 305 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "./auth/auth.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  loadedFeature = 'recipe';
+export class AppComponent implements OnInit {
+  constructor(private authservice: AuthService) {}
 
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
+  ngOnInit(): void {
+    this.authservice.autoLogin();
   }
 }
